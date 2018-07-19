b17ad20b3f650daa79715908a858187ef73cf9a8
Varun Malik <varun1malik@gmail.com>
2018-07-19 17:06:30 +0530

    transform sol

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,13 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static List<int> Map(this int[] numbers, Func<int, int> operation)
         {
-            throw new NotImplementedException();
+            List<int> operation_list = new List<int>();
+            foreach(int digit in numbers){
+              operation_list.Add(operation(digit));
+            }
+            return operation_list;
         }
     }
 }
